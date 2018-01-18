f38886a3361cf6dac3818fa29ddc2dda84314850
Kiara <deleon_k@ymail.com>
2018-01-18 18:35:59 -0500

    Recursion is weird

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int destination = 3;
-        int start = 1;
-        int spare = 2;
-
-        System.out.print("The world depends on you! Tell me how many discs we're dealing with.");
+        System.out.println("The world depends on you! Tell me how many discs we're dealing with.");
         Scanner discInput = new Scanner(System.in);
         int discNum = discInput.nextInt();
 
         Hanoi discs = new Hanoi();
-        discs.buildTower(discNum, start, spare, destination);
+        discs.buildTower(discNum, 1, 2, 3);
 
     }
 }
